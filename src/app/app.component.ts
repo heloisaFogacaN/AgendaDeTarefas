cddb3fe87a801ee5ad1dc2be40460ea24114b6f6
heloisaFogacaN <heloisa_nascimento1@estudante.sesisenai.org.br>
2023-05-12 16:50:56 -0300

    Implementa segmentos da Terceira Entrega

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { log } from 'console';
 
 
 interface Tarefa{
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { TarefasComponent } from './tarefas/tarefas.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TarefasComponent,
+    CategoriasComponent
   ],
   imports: [
     BrowserModule,
