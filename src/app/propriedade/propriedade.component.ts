00825b4cd9cdd13577805a494700dbb52cbdb018
heloisaFogacaN <heloisa_nascimento1@estudante.sesisenai.org.br>
2023-06-19 16:53:44 -0300

    Fazendo a quinta entrega

--- src/app/propriedade/propriedade.component.ts
+++ src/app/propriedade/propriedade.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+  interface Propriedade{
+  nome : string
+  tipo : string
+  dado : any
+  }
+
 @Component({
   selector: 'app-propriedade',
   templateUrl: './propriedade.component.html',
@@ -10,22 +16,44 @@ export class PropriedadeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const propriedades = localStorage.getItem('propriedade');
+    if (propriedades) {
+      this.propriedadeLista = JSON.parse(propriedades);
+    }
+    console.log(propriedades);
   }
-  novaPropriedade: string;
-  propriedades: string[] = [];
+
+  propriedadeLista: string[] = [];
   tipoPropriedade: string;
   nomePropriedade: string;
+  propriedade : string;
+  novaPropriedade: Propriedade ={
+    nome: '',
+    tipo: '',
+    dado: null
+  }
 
   cadastrarPropriedade(){
     console.log(this.novaPropriedade)
-    this.propriedades.push(this.novaPropriedade);
-    console.log(this.propriedades)  
+    const propriedade : Propriedade = {
+      nome:this.novaPropriedade.nome,
+      tipo:this.novaPropriedade.tipo,
+      dado:this.novaPropriedade.dado
+    }
+    this.propriedadeLista.push(this.propriedade);
+    console.log(this.propriedadeLista)  
     this.salvarLocalStorage()
-    this.novaPropriedade='';
+    this.propriedade='';
+  }
+
+  removerCategoria(indice: number){
+    this.propriedadeLista.splice(indice, 1);
+    this.salvarLocalStorage()
+
   }
 
   salvarLocalStorage(){
-    localStorage.setItem("propriedade",JSON.stringify(this.propriedades))
+    localStorage.setItem("propriedade",JSON.stringify(this.propriedadeLista))
   }
 
 }
